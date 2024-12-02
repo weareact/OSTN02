f4e2bd1c53c216e213f8ea7133a603968be00311
leyatesfujitsu <lewis.harper-yates@fujitsu.com>
2024-12-02 16:08:44 +0000

    [IB-218] - Add toolchains and update to JDK17

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,7 @@
 library('common-pipeline')
 buildProperties.getPropertyBuilderWithDefaults()
         .emailRecipients('cardiff-dev@weareact.com')
+        .jdkVersion('OpenJDK17-Latest')
         .buildAndSet()
 sampleScripts.simpleJavaProject(nodeName: 'docker-support', timeout: 80)
 sampleScripts.releaseIfMasterOrIfReleaseParameter(nodeName: 'docker-support', timeout: 80)
